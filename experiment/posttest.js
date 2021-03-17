306bf0a3cf89c185e951e3d67519e8656856c356
ojas1 <mohrilop@gmail.com>
2021-03-17 20:39:55 +0530

    quiz fix

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -5,149 +5,150 @@
 /////////////////////////////////////////////////////////////////////////////
 
 (function() {
-    function buildQuiz() {
-        // we'll need a place to store the HTML output
-        const output = [];
+function buildQuiz() {
+// we'll need a place to store the HTML output
+const output = [];
 
-        // for each question...
-        myQuestions.forEach((currentQuestion, questionNumber) => {
-            // we'll want to store the list of answer choices
-            const answers = [];
+// for each question...
+myQuestions.forEach((currentQuestion, questionNumber) => {
+// we'll want to store the list of answer choices
+const answers = [];
 
-            // and for each available answer...
-            for (letter in currentQuestion.answers) {
-                // ...add an HTML radio button
-                answers.push(
-                    `<label>
-          <input type="radio" name="question${questionNumber}" value="${letter}">
-          ${letter} :
-          ${currentQuestion.answers[letter]}
-        </label>`
-                );
-            }
+// and for each available answer...
+for (letter in currentQuestion.answers) {
+// ...add an HTML radio button
+answers.push(
+`<label>
+<input type="radio" name="question${questionNumber}" value="${letter}">
+${letter} :
+${currentQuestion.answers[letter]}
+</label>`
+);
+}
 
-            // add this question and its answers to the output
-            output.push(
-                `<div class="question"> ${currentQuestion.question} </div>
-      <div class="answers"> ${answers.join("")} </div>`
-            );
-        });
+// add this question and its answers to the output
+output.push(
+`<div class="question"> ${currentQuestion.question} </div>
+<div class="answers"> ${answers.join("")} </div>`
+);
+});
 
-        // finally combine our output list into one string of HTML and put it on the page
-        quizContainer.innerHTML = output.join("");
-    }
+// finally combine our output list into one string of HTML and put it on the page
+quizContainer.innerHTML = output.join("");
+}
 
-    function showResults() {
-        // gather answer containers from our quiz
-        const answerContainers = quizContainer.querySelectorAll(".answers");
+function showResults() {
+// gather answer containers from our quiz
+const answerContainers = quizContainer.querySelectorAll(".answers");
+answerContainers.forEach(e => e.style.color = "black");
 
-        // keep track of user's answers
-        let numCorrect = 0;
+// keep track of user's answers
+let numCorrect = 0;
 
-        // for each question...
-        myQuestions.forEach((currentQuestion, questionNumber) => {
-            // find selected answer
-            const answerContainer = answerContainers[questionNumber];
-            const selector = `input[name=question${questionNumber}]:checked`;
-            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+// for each question...
+myQuestions.forEach((currentQuestion, questionNumber) => {
+// find selected answer
+const answerContainer = answerContainers[questionNumber];
+const selector = `input[name=question${questionNumber}]:checked`;
+const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-            // if answer is correct
-            if (userAnswer === currentQuestion.correctAnswer) {
-                // add to the number of correct answers
-                numCorrect++;
+// if answer is correct
+if (userAnswer === currentQuestion.correctAnswer) {
+// add to the number of correct answers
+numCorrect++;
 
-                // color the answers green
-                //answerContainers[questionNumber].style.color = "lightgreen";
-            } else {
-                // if answer is wrong or blank
-                // color the answers red
-                answerContainers[questionNumber].style.color = "red";
-            }
-        });
+// color the answers green
+//answerContainers[questionNumber].style.color = "lightgreen";
+} else {
+// if answer is wrong or blank
+// color the answers red
+answerContainers[questionNumber].style.color = "red";
+}
+});
 
-        // show number of correct answers out of total
-        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-    }
+// show number of correct answers out of total
+resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
 
-    const quizContainer = document.getElementById("quiz");
-    const resultsContainer = document.getElementById("results");
-    const submitButton = document.getElementById("submit");
-
-
-    /////////////////////////////////////////////////////////////////////////////
-
-    /////////////////////// Do not modify the above code ////////////////////////
-
-    /////////////////////////////////////////////////////////////////////////////
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("submit");
 
 
+/////////////////////////////////////////////////////////////////////////////
 
+/////////////////////// Do not modify the above code ////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////
 
 
 
-    /////////////// Write the MCQ below in the exactly same described format ///////////////
 
 
-    const myQuestions = [{
-            question: "1. Select one of the applications of Depth First Search?", ///// Write the question inside double quotes
-            answers: {
-                a: "Generating topological sort of a graph ", ///// Write the option 1 inside double quotes
-                b: "Generating strongly connected components of a directed graph ", ///// Write the option 2 inside double quotes
- 		c: "Detecting cycles in the graph ", ///// Write the option 3 inside double quotes
-                d: " All of these", ///// Write the option 4 inside double quotes
-            },
-            correctAnswer: "a" ///// Write the correct option inside double quotes
-        },
 
-    {
-      question: "2.  In Depth First Search, how many times does a node visited?",  ///// Write the question inside double quotes
-      answers: {
-        a: "Once",                  ///// Write the option 1 inside double quotes
-        b: "Twice ",                  ///// Write the option 2 inside double quotes
-	c: " Equivalent to number of indegree of the node", ///// Write the option 3 inside double quotes
-        d: "None of these ", ///// Write the option 4 inside double quotes
-              },
-      correctAnswer: "a"                ///// Write the correct option inside double quotes
-    },
+/////////////// Write the MCQ below in the exactly same described format ///////////////
+
+
+const myQuestions = [{
+question: "1. Select one of the applications of Depth First Search?", ///// Write the question inside double quotes
+answers: {
+a: "Generating topological sort of a graph ", ///// Write the option 1 inside double quotes
+b: "Generating strongly connected components of a directed graph ", ///// Write the option 2 inside double quotes
+c: "Detecting cycles in the graph ", ///// Write the option 3 inside double quotes
+d: " All of these", ///// Write the option 4 inside double quotes
+},
+correctAnswer: "a" ///// Write the correct option inside double quotes
+},
 
 {
-      question: "3. While running DFS on a directed graph, if from vertex 'u', we visit a finished vertex 'v', then the edge(u,v)is a cross-edge. ",  ///// Write the question inside double quotes
-      answers: {
-        a: "True",                  ///// Write the option 1 inside double quotes
-        b: "False",                  ///// Write the option 2 inside double quotes
-	
-              },
-      correctAnswer: "a"                ///// Write the correct option inside double quotes
-    },
+question: "2.  In Depth First Search, how many times does a node visited?",  ///// Write the question inside double quotes
+answers: {
+a: "Once",                  ///// Write the option 1 inside double quotes
+b: "Twice ",                  ///// Write the option 2 inside double quotes
+c: " Equivalent to number of indegree of the node", ///// Write the option 3 inside double quotes
+d: "None of these ", ///// Write the option 4 inside double quotes
+},
+correctAnswer: "a"                ///// Write the correct option inside double quotes
+},
 
 {
-      question: "4.All Graphs have unique representation on paper.",  ///// Write the question inside double quotes
-      answers: {
-        a: "True",                  ///// Write the option 1 inside double quotes
-        b: "False",                  ///// Write the option 2 inside double quotes
-              },
-      correctAnswer: "b"                ///// Write the correct option inside double quotes
-    },
+question: "3. While running DFS on a directed graph, if from vertex 'u', we visit a finished vertex 'v', then the edge(u,v)is a cross-edge. ",  ///// Write the question inside double quotes
+answers: {
+a: "True",                  ///// Write the option 1 inside double quotes
+b: "False",                  ///// Write the option 2 inside double quotes
+
+},
+correctAnswer: "a"                ///// Write the correct option inside double quotes
+},
+
+{
+question: "4.All Graphs have unique representation on paper.",  ///// Write the question inside double quotes
+answers: {
+a: "True",                  ///// Write the option 1 inside double quotes
+b: "False",                  ///// Write the option 2 inside double quotes
+},
+correctAnswer: "b"                ///// Write the correct option inside double quotes
+},
 
 
-        
-    ];
+
+];
 
 
 
 
-    /////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 
-    /////////////////////// Do not modify the below code ////////////////////////
+/////////////////////// Do not modify the below code ////////////////////////
 
-    /////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 
 
-    // display quiz right away
-    buildQuiz();
+// display quiz right away
+buildQuiz();
 
-    // on submit, show results
-    submitButton.addEventListener("click", showResults);
+// on submit, show results
+submitButton.addEventListener("click", showResults);
 })();
 
 
